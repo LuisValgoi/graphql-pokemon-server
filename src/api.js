405c85604f87da257391c71b21126e8fcb5fee4a
Luis Valgoi <luis.valgoi@sap.com>
2020-07-03 02:17:22 -0300

    fix: removed cors

--- src/api.js
+++ src/api.js
@@ -6,6 +6,13 @@ const router = express.Router();
 
 const data = require('./data/GET_POKEMONS');
 router.get('/v1/pokemons', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.json(data);
+});
+router.get('/v1/pokemons/:id', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.json(data);
 });
 app.use('/.netlify/functions/api', router)
