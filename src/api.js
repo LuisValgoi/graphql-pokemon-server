299a32d5cde283c9d75dd97a83e1b8b3dcc01184
Luis Valgoi <luis.valgoi@sap.com>
2020-07-03 02:40:50 -0300

    fix: url match

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ router.get('/v1/pokemons', (req, res) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.json(data);
 });
-router.get('/v1/pokemons/:id', (req, res) => {
+router.get('/v1/pokemons/*', (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.json(data);
