f1bab961a364a12965cc74ef94079563bdfb40a3
Luis Valgoi <luis.valgoi@sap.com>
2020-07-04 15:57:44 -0300

    feat: added filter api

--- src/api.js
+++ src/api.js
@@ -9,6 +9,18 @@ const data = require('./data/GET_POKEMONS');
 router.get('/v1/pokemons', (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  const nameParam = req.query.query;
+
+  if (nameParam) {
+    const response = {
+      data: {
+        pokemons: data.data.pokemons.filter(pokemon => pokemon.name.toLowerCase().trim().includes(nameParam.toLowerCase()))
+      }
+    }
+    res.json(response);
+  }
+
   res.json(data);
 });
 
